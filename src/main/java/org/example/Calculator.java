3d9ec64be5cbd98b79179a9a21331070c8bfff81
Waleed Abu Qalben <waleed.abu-qalben@globitel.com>
2023-03-13 16:32:39 +0200

    added div and multi with log4j

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,23 +1,43 @@
 package org.example;
 
-public class Calculator {
 
-   public static int sum (int x, int y) {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Calculator {
+    private static Logger logger = LogManager.getLogger(Calculator.class.getName());
+    public static int sum (int x, int y) {
        return x + y;
    }
    public static int sub (int x, int y) {
        return x - y ;
    }
+   public static int multi (int x, int y) {return x * y ;}
+   public static int div (int x, int y) {
+        if (y == 0) {
+            logger.error("denominator must be greater than 0");
+            throw new ArithmeticException("denominator must be greater than 0");
+        }
+        return x / y;
+    }
    public static int calculate (String operation, int x, int y) {
+
        operation = operation.trim();
        switch (operation) {
-           case "sum" -> {
+           case "sum" :
+               logger.info("Summing -> "+x+" + "+y);
                return sum(x, y);
-           }
-           case "sub" -> {
+           case "sub" :
+               logger.info("Subtraction -> "+x+" - "+y);
                return sub(x, y);
-           }
-           default -> throw new IllegalArgumentException();
+           case "multi" :
+               logger.info("Multiplying -> "+x+" * "+y);
+               return multi(x, y);
+           case "div" :
+               logger.info("Dividing -> "+x+" / "+y);
+               return div(x, y);
+
+           default : throw new IllegalArgumentException();
        }
    }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
-
 import java.util.Scanner;
+
+
+
 
 public class Main {
     public static void main(String[] args) {
+
+
         Scanner scan = new Scanner(System.in);
         String operation = scan.next();
         int x = scan.nextInt();
